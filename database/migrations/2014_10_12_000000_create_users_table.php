b58afa2e43d863b103d412e4bdd44443b5e10c93
dmendoza064 <dmendoza064@gmail.com>
2017-12-07 21:32:46 -0600

    configuracion de bd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -15,9 +14,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name', 100);
+            $table->string('last_name', 100);
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('user', 50)->unique();
+            $table->string('password', 60);
+            $table->enum('type', ['user', 'admin']);
+            $table->boolean('active');
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
