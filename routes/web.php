3640344e342d08ab450efb2e57ec38fe97628a13
dmendoza064 <dmendoza064@gmail.com>
2017-12-07 22:22:58 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,90 @@ Route::group(['middleware' => 'auth'], function () {
     #
     #
 });
+Route::bind('product', function($slug){
+	return App\Product::where('slug', $slug)->first();
+});
+
+// Category dependency injection
+Route::bind('category', function($category){
+    return App\Category::find($category);
+});
+
+// User dependency injection
+Route::bind('user', function($user){
+    return App\User::find($user);
+});
+
+
+Route::get('/', [
+	'as' => 'home',
+	'uses' => 'StoreController@index'
+]);
+
+Route::get('product/{slug}', [
+	'as' => 'product-detail',
+	'uses' => 'StoreController@show'
+]);
+
+// Carrito -------------
+
+Route::get('cart/show', [
+	'as' => 'cart-show',
+	'uses' => 'CartController@show'
+]);
+
+Route::get('cart/add/{product}', [
+	'as' => 'cart-add',
+	'uses' => 'CartController@add'
+]);
+
+Route::get('cart/delete/{product}',[
+	'as' => 'cart-delete',
+	'uses' => 'CartController@delete'
+]);
+
+Route::get('cart/trash', [
+	'as' => 'cart-trash',
+	'uses' => 'CartController@trash'
+]);
+
+Route::get('cart/update/{product}/{quantity}', [
+	'as' => 'cart-update',
+	'uses' => 'CartController@update'
+]);
+
+Route::get('order-detail', [
+	'middleware' => 'auth:user',
+	'as' => 'order-detail',
+	'uses' => 'CartController@orderDetail'
+]);
+
+Route::group(['namespace' => 'Admin', 'middleware' => ['auth'], 'prefix' => 'admin'], function()
+{
+
+	Route::get('home', function(){
+		return view('admin.home');
+	});
+
+	Route::resource('category', 'CategoryController');
+
+	Route::resource('product', 'ProductController');
+
+	Route::resource('user', 'UserController');
+
+	Route::get('orders', [
+		'as' => 'admin.order.index',
+		'uses' => 'OrderController@index'
+	]);
+
+	Route::post('order/get-items', [
+	    'as' => 'admin.order.getItems',
+	    'uses' => 'OrderController@getItems'
+	]);
+
+	Route::get('order/{id}', [
+	    'as' => 'admin.order.destroy',
+	    'uses' => 'OrderController@destroy'
+	]);
+
+});
